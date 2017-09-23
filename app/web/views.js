710b4ccbdc1a3850b5831f3334be0ef4b5694c14
Emerald Lewis <lewiemer@isu.edu>
2017-09-23 14:30:44 -0600

    Worked on news portion of the site

--- app/web/views.js
+++ app/web/views.js
@@ -1,13 +1,24 @@
 'use strict';
 
 const express = require('express');
+const request = require('request-promise');
 
 function views(config) {
     var config = config || {},
         router = express.Router();
-
+    var api_url = config.api_url || 'http://localhost:8081';
     router.get('/', function (req, res) {
-        res.render('index.mustache');
+        request(api_url + "/news", { json: true })
+            .then(function (data) {
+                console.log(data);
+                res.render('index.mustache', { news : data});
+
+            })
+            .catch(function (err) {
+                console.log(err);
+                res.send(err);
+            });
+        
     });
 
     router.get('/signup', function (req, res) {
